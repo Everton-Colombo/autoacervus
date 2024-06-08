76fbe23392bceaaf23c22ae182c8db8add46712b
romcol <e.rcolombo2@gmail.com>
2024-06-08 15:06:46 -0300

    Basic logging

--- src/main/java/com/example/autoacervus/service/EmailServiceImpl.java
+++ src/main/java/com/example/autoacervus/service/EmailServiceImpl.java
@@ -2,13 +2,18 @@ package com.example.autoacervus.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class EmailServiceImpl implements EmailService {
+
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
 
     @Autowired
     private JavaMailSender emailSender;
@@ -18,12 +23,17 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendSimpleMail(String to, String subject, String content) {
+        logger.info("[sendSimpleMail()] subject: \"%s\"; to: %s]".formatted(subject, to));
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(this.fromMail);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(content);
-        emailSender.send(message);
+        try {
+            emailSender.send(message);
+        } catch (MailException e) {
+            logger.severe("[sendSimpleMail()] Failed to send simple mail. Error msg: " + e.getMessage());
+        }
     }
 
     @Override
