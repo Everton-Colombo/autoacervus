720c17157701c676bdc4093fa2ccdaa42c41d57d
romcol <e.rcolombo2@gmail.com>
2024-06-09 00:40:06 -0300

    Add new index page

--- src/main/java/com/example/autoacervus/controller/HomeAndInfoController.java
+++ src/main/java/com/example/autoacervus/controller/HomeAndInfoController.java
@@ -1,13 +1,16 @@
 package com.example.autoacervus.controller;
 
+import com.example.autoacervus.model.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeAndInfoController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("newUser", new User());
         return "index";
     }
 }
