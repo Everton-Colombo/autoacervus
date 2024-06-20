dc48888151bae018dbae655dae97fd798058f818
lucasrdrgs <lucasrodriguespimentel@gmail.com>
2024-06-20 20:23:30 -0300

    Lint

--- src/main/java/com/example/autoacervus/dao/UserDAO.java
+++ src/main/java/com/example/autoacervus/dao/UserDAO.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface UserDAO extends CrudRepository<User, String> {
 
-    User findByEmailDac(String emailDac);   // Automagically implemented by Spring Data JPA
+    User findByEmailDac(String emailDac); // Automagically implemented by Spring Data JPA
 
     @Query("SELECT user FROM User user JOIN user.borrowedBooks borrowedBook " +
             "WHERE borrowedBook.expectedReturnDate = CURRENT_DATE")
